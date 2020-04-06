bbe6680bbe4f824b92cb47703dfa8c8288fd3eac
Rahul Bansal <bansal.rahul14@gmail.com>
2020-04-06 12:15:57 +0530

    fixed the experience

--- src/data/candidatedata.js
+++ src/data/candidatedata.js
@@ -1,7 +1,7 @@
 const candidatedata = [
     {
         role:'Marketing',
-        exp:'4 years',
+        exp:'< 1',
         college:'IIM Rohtak',
         tags:['finance', 'marketing','strategy','MS Excel', 'MS Word', 'C++', 'Java', 'Salesforce', 'GitHub', 'PowerPoint'],
         summary: 'I am Shini Jain, first year MBA student at Indian Institute of Management, Rohtak. I am a computer engineer and have work experience in Accenture for 23 months in technology. Due to the ongoing COVID-19 pandemic, the internship I was offered stands cancelled. I am actively looking for internship opportunities in varied domains and my areas of interest are Marketing, Research, Strategy, Consulting and Finance. Pursuing summer internship from your organization will enrich my learning and give me the necessary exposure. The duration Iamm looking for the internship is 6-8 weeks i.e., from April to May.' ,
@@ -17,7 +17,7 @@ const candidatedata = [
         tags:['Python', 'C++', 'node.js','typescript', 'django', 'REST API', 'PostgreSQL', 'Bigquery', 'S3', 'SQS', 'SES'],
         summary: "I am a hard working and quick learner, always keen on learning new thing. Looking for a software developer or full stack developer role. Current experience is of 9 months.",
         email:'singh.gunjan72@gmail.com',
-        linkedin:'https://www.linkedin.com/gunjansingh727'
+        linkedin:'https://www.linkedin.com/in/gunjansingh727'
       },
 ];
 
